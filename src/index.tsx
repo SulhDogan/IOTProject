4f7e796ae09faf82fde326c6edc9d13078f4819c
Barış Doğan <sulhdogan@gmail.com>
2020-01-15 20:17:10 +0300

    iot screen & cloud data

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from "firebase";
 import { Globals } from './connection';
+import { firebaseConfig } from "./cloud";
 
-let firebaseConfig = {
-    apiKey: "AIzaSyAjv32x6wNIWIeQwwHaAjKzB9P__8zD7JU",
-    authDomain: "technocoop-f0398.firebaseapp.com",
-    databaseURL: "https://technocoop-f0398.firebaseio.com",
-    projectId: "technocoop-f0398",
-    storageBucket: "technocoop-f0398.appspot.com",
-    messagingSenderId: "821315922742",
-    appId: "1:821315922742:web:f333254b8ec57f7290e197"
-};
 let app = firebase.initializeApp(firebaseConfig);
 Globals.firestore = firebase.firestore();
 
